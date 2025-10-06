75ebeebfabe571a8d0f678b7cafa2812e998d01e
thenamelessdev <aron.minarovits@gmail.com>
2025-10-06 18:48:01 +0200

    started coding

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config;
+const app = express();
+app.use("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+const apiRouter = require("./routes/api");
+app.use("/api", apiRouter);
+
+app.listen(80);
