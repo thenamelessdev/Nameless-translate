d4cfe707393d194f7336642f0467bc6047e1231f
thenamelessdev <aron.minarovits@gmail.com>
2025-10-17 12:17:10 +0200

    sql injection prevention

--- log.js
+++ log.js
@@ -2,7 +2,7 @@ const dbConn = require("./db");
 
 async function logTranslate(targetLang, text, ip) {
     try{
-        await dbConn.query("INSERT INTO `logs`(`targetLang`, `text`, `ip`) VALUES ('" + targetLang + "','" + text + "','" + ip + "')");
+        await dbConn.query("INSERT INTO `logs`(`targetLang`, `text`, `ip`) VALUES ('?','?','?')", [targetLang, text, ip]);
         return;
     }
     catch(err){
